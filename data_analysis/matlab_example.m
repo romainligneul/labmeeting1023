81520d2f3c5649a6e2f199f10d0d1fb34d2aa544
Romain <romain.ligneul@gmail.com>
2023-11-06 11:40:05 +0100

    stats

---
+++ data_analysis/matlab_example.m
@@ -0,0 +1,28 @@
+clear all
+
+folder_stats_json='stats';
+%% t-test example
+clear test
+load stockreturns
+x = stocks(:,3);
+[test.h,test.p,test.ci,test.stats] = ttest(x);
+% save the test as json
+testname='ttest_stock';
+encodedStats=jsonencode(test);
+fid = fopen([folder_stats_json '\' testname '.json'],'w');
+fprintf(fid,'%s',encodedStats);
+fclose(fid);
+
+%% anova example
+clear test
+load hogg
+hogg
+[test.p,test.tbl,test.stats] = anova1(hogg,[],'off');
+
+% save the test as json
+testname='anova_hogg';
+encodedStats=jsonencode(test);
+fid = fopen([folder_stats_json '\' testname '.json'],'w');
+fprintf(fid,'%s',encodedStats);
+fclose(fid);
+
---
+++ test.js
@@ -0,0 +1,2 @@
+const statdata = FileAttachment("stats_dummyExample.csv").csv({ typed: true });
+console.log(statdata.columns);
